9634cc30fa7692952dd5047775f5aed6952e9a49
Aayush Agrawal <4214851+aayushagrawal9@users.noreply.github.com>
2026-04-05 18:18:25 -0700

    Add gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://aayushagrawal9.github.io',
+  base: '/gdg-indore-aipl',
   integrations: [tailwind({ applyBaseStyles: false })],
   server: { host: '127.0.0.1', port: 4321 },
 });
