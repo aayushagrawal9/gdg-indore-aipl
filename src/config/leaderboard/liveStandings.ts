e390c854c91b77184f8722d3e6f18d21e2818ae8
priyaljain919 <priyaljain919@gmail.com>
2026-04-13 22:14:14 +0530

    updated all data into leaderboard page

--- src/config/leaderboard/liveStandings.ts
+++ src/config/leaderboard/liveStandings.ts
@@ -34,11 +34,109 @@ export const liveStandings = {
       accent: 'tertiary'
     },
     {
-      rank: '04',
-      name: 'Munish jain',
-      description: '',
-      vibeIndex: 25,
-      accent: 'tertiary'
-    },
+    rank: '04',
+    name: 'Munish jain',
+    description: '',
+    vibeIndex: 25,
+    accent: 'tertiary'
+  },
+  {
+    rank: '05',
+    name: 'Build Algos',
+    description: '',
+    vibeIndex: 24,
+    accent: 'tertiary'
+  },
+  {
+    rank: '06',
+    name: 'Codex',
+    description: '',
+    vibeIndex: 24,
+    accent: 'tertiary'
+  },
+  {
+    rank: '07',
+    name: 'Team ethics',
+    description: '',
+    vibeIndex: 23,
+    accent: 'tertiary'
+  },
+  {
+    rank: '08',
+    name: 'Kushagra misra',
+    description: '',
+    vibeIndex: 23,
+    accent: 'tertiary'
+  },
+  {
+    rank: '09',
+    name: 'StadiumPulse',
+    description: '',
+    vibeIndex: 23,
+    accent: 'tertiary'
+  },
+  {
+    rank: '10',
+    name: 'Ethics',
+    description: '',
+    vibeIndex: 22,
+    accent: 'tertiary'
+  },
+  {
+    rank: '11',
+    name: 'Samyak choudhary ',
+    description: '',
+    vibeIndex: 22,
+    accent: 'tertiary'
+  },
+  {
+    rank: '12',
+    name: 'Hustlers',
+    description: '',
+    vibeIndex: 21,
+    accent: 'tertiary'
+  },
+  {
+    rank: '13',
+    name: 'Faluda',
+    description: '',
+    vibeIndex: 21,
+    accent: 'tertiary'
+  },
+  {
+    rank: '14',
+    name: 'Byte Avengers ',
+    description: '',
+    vibeIndex: 17,
+    accent: 'tertiary'
+  },
+  {
+    rank: '15',
+    name: 'Rockcode',
+    description: '',
+    vibeIndex: 17,
+    accent: 'tertiary'
+  },
+  {
+    rank: '16',
+    name: 'Samyak Choudhary ',
+    description: '',
+    vibeIndex: 10,
+    accent: 'tertiary'
+  },
+  {
+    rank: '17',
+    name: 'Dropcode ',
+    description: '',
+    vibeIndex: 9,
+    accent: 'tertiary'
+  },
+  {
+    rank: '18',
+    name: 'UP187 ',
+    description: '',
+    vibeIndex: 9,
+    accent: 'tertiary'
+  }
   ] satisfies LiveTeam[],
 };
